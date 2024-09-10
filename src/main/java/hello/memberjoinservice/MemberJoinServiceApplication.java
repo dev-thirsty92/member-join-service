05b63e927c1d5409f1b0970ea28db6c11bf874a6
needsomewater <needsomewater@naver.com>
2024-09-11 01:20:09 +0900

    init

---
+++ src/main/java/hello/memberjoinservice/MemberJoinServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.memberjoinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberJoinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberJoinServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/memberjoinservice/MemberJoinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.memberjoinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberJoinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
