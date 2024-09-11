13c26f4aea00a967e07e21f4c8ac2aa6df2a583e
beta1992 <beta1992@smartport.kr>
2024-09-11 22:58:39 +0900

    01_pojo TDD

--- src/test/java/hello/memberjoinservice/member/MemberJoinServiceTest.java
+++ src/test/java/hello/memberjoinservice/member/MemberJoinServiceTest.java
@@ -16,22 +16,24 @@ class MemberJoinServiceTest {
 
     @Test
     void 회원가입() {
+        final MemberJoinRequest request = 회원가입요청_생성();
+        memberJoinService.signUp(request);
+    }
+
+    private static MemberJoinRequest 회원가입요청_생성() {
         String username = "user00";
         String password = "passWord00";
-        MemberJoinRequest request = new MemberJoinRequest(username, password);
-        memberJoinService.signUp(request);
+        return new MemberJoinRequest(username, password);
     }
 
     @Test
     void 회원가입_중복() {
-        String username = "user00";
-        String password = "passWord00";
+        final MemberJoinRequest request1 = 회원가입요청_생성();
         // db에 중복된 username이 없으므로 회원을 저장
-        MemberJoinRequest request1 = new MemberJoinRequest(username, password);
         memberJoinService.signUp(request1);
 
         // db에 중복된 username이 있으므로 저장 실패
-        MemberJoinRequest request2 = new MemberJoinRequest(username, password);
+        final MemberJoinRequest request2 = 회원가입요청_생성();
         memberJoinService.signUp(request2);
 
     }
